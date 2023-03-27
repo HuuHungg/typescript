6fd12a928fd6388db9ebae3397ea59f9d6a53127
Huu Hung <huuhungnguyen2002@gmail.com>
2023-03-27 20:51:03 +0700

    add file modfile

---
+++ lesson4/lesson4.js
@@ -0,0 +1,9 @@
+const sum =  (x,y) => {
+    return x + y
+}
+console.log("Check sum", sum(20,8))
+
+const sum2 = (x,y) => {
+    return x + y
+}
+console.log("Check sum2", sum2("Number", 28))
---
+++ lesson4/lesson4.ts
@@ -0,0 +1,8 @@
+const sum3 = (x:number, y:number) => {
+    return x + y
+}
+
+// console.log(">>> checksum 3", sum3(20,40))
+console.log(">>> check sum3 =", sum3(5,"15"))
+
+// error
