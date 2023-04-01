f07fcfd72bb952d6c21355cbc8f342f76e7cb3c1
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-01 22:17:15 +0700

    add file modfile

--- lesson6/lesson6.ts
+++ lesson6/lesson6.ts
@@ -1,39 +1,61 @@
-let count2: number 
-
-count2 = 10
-
-console.log("Check code", count2)
-
-let name2 : string[] = ["Javascript", "PHP" , "Ruby"]
-
-name2.push("HTML&CSS")
-
-console.log(name2)
-
-let myVar : string | number = "Hello"
-
-
-console.log(myVar)
-
-let num :number = 10;
-let str: string = "Hello"
-
-function add(num1: number, num2: number) {
-    return num1 + num2
-}
-
-let result = add(10,20)
-
-console.log(result)
-
-interface Person {
-    name: string
-    age: number
-}
-
-const viblo : Person = {
-    name: "毎日ITを勉強してる頑張りましょう",
-    age : 21
-}
-
-console.log(viblo)
+interface User {
+    name: string;
+    age: number;
+    email: string;
+    isAdmin: boolean;
+  }
+  
+  interface UserManager {
+    getUsers(): User[];
+    getUserById(id: string): User | undefined;
+    addUser(user: User): void;
+    removeUserById(id: string): void;
+  }
+  
+  class UserDatabase implements UserManager {
+    private users: User[] = [];
+  
+    getUsers(): User[] {
+      return this.users;
+    }
+  
+    getUserById(id: string): User | undefined {
+      return this.users.find(user => user.email === id);
+    }
+  
+    addUser(user: User): void {
+      this.users.push(user);
+    }
+  
+    removeUserById(id: string): void {
+      this.users = this.users.filter(user => user.email !== id);
+    }
+  }
+  
+  const database = new UserDatabase();
+  
+  const user1: User = {
+    name: "John Doe",
+    age: 30,
+    email: "johndoe@example.com",
+    isAdmin: false
+  };
+  
+  database.addUser(user1);
+  
+  console.log(database.getUsers());
+  // Output: [{name: "John Doe", age: 30, email: "johndoe@example.com", isAdmin: false}]
+  
+  const user2: User = {
+    name: "Jane Doe",
+    age: 25,
+    email: "janedoe@example.com",
+    isAdmin: true
+  };
+  
+  database.addUser(user2);
+  
+  console.log(database.getUsers());
+  // Output: [{name: "John Doe", age: 30, email: "johndoe@example.com", isAdmin: false},
+  //          {name: "Jane Doe", age: 25, email: "janedoe@example.com", isAdmin: true}]
+  
