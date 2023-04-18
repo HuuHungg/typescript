7b9ba3190f170d19f0dd81b4e8326977697a6a6f
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-18 15:59:03 +0700

    add file modifile

--- src/main.ts
+++ src/main.ts
@@ -1,73 +1,94 @@
-let stringArr = ['one', 'hey', 'Dave']
+type stringOrNumber = string | number
 
-let guitar = ['Start', 'Les Paul', 5250]
+type stringOrNumberArray = (string | number)[]
 
-let mixedData = ['EVH', 1980, true]
-
-stringArr[0] = 'John'
-stringArr.push('hey')
-
-
-guitar[0] = 1984
-guitar.unshift('Jim')
-
-let test = []
-let bands: string[] = []
-bands.push('Van Halen')
-
-
-
-let myTuple: [string, number, boolean] = ['Dave', 42, true]
-let mixed = ['John', 1, false]
-myTuple[1] = 82
-
-let myObj: object
-myObj = []
-console.log(typeof myObj)
-myObj = bands
-myObj = {}
-
-const exampleObj = {
-    prop1: 'Dave',
-    prop2: true,
-}
-
-exampleObj.prop1 = 'John'
 
 interface Guitarist {
     name ?: string,
     active: boolean,
-    albums: (string | number)[]
+    albums: stringOrNumberArray
 }
 
-let evh: Guitarist = {
-    name: "Eddie",
-    active: false,
-    albums: [1984, 5150, 'asddd']
+type UserId = stringOrNumber
+
+// literal types
+
+let myName: "Dave"
+
+let UserName: "Dave" | "John" | "Amy"
+UserName = "Amy"
+
+// function 
+const add = (a: number,b:number): number => {
+    return a + b
 }
 
-let JP: Guitarist = {
-    name: 'Jimmy',
-    active: true,
-    albums: ['JH','Name']
+console.log(add(10,20))
+
+const logMsg = (message: any):void => {
+    console.log(message)
 }
 
-const greetGuitarist =(guitarist: Guitarist) => {
-    if(guitarist.name) {
-        return `Hello ${guitarist.name?.toUpperCase()}!`
+logMsg("Hello!")
+
+
+let subtract = function (c: number, d: number): number {
+    return c - d
+}
+
+// type mathFunction = (a: number, b: number) => number
+
+interface mathFunction {
+    (a: number, b: number): number
+}
+
+let multiply: mathFunction = function (c,d) {
+    return c * d
+}
+
+logMsg(multiply(20,20))
+
+
+// optional parameter
+const addAll = (a: number, b: number, c?: number):number => {
+    if( typeof c !== "undefined") {
+        return a + b + c 
+    }   
+    return a + b
+}
+
+
+const sumAll = (a: number = 20, b: number, c: number = 10): number => {
+    return a + b + c
+}
+
+
+logMsg(addAll(2,2,2))
+logMsg(add(2,2))
+logMsg(sumAll(2,2))
+logMsg(sumAll(undefined, 3))
+
+console.log("===========")
+
+// Rest Parameter
+const total = (a:number, ...nums: number[]): number => {
+    return a + nums.reduce((prev, curr) => prev + curr)
+}
+
+logMsg(total(10,2,3,4,5))
+
+
+const isNumber = (value: any):boolean => {
+    return typeof value === 'number'
+        ? true: false
+}
+
+const numberOrString = (value: number | string) => {
+    if(typeof value === 'string') {
+        return "This is the string"
+    }   
+    if(typeof value === 'number') {
+        return "This is the number"
     }
-    return 'Hello!'
 }
-
-console.log(greetGuitarist(JP))
-
-// Enums 
-
-enum Grade {
-    U = "Huu Hung Nguyen",
-    D = 1,
-    C = "Face"
-}
-
-console.log(Grade.C)
 
