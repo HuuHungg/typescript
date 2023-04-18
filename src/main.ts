2cf4166ffa23f24c53db4d6614af47a48f9a82f8
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-18 21:52:18 +0700

    add file modifile

--- src/main.ts
+++ src/main.ts
@@ -1,94 +1,32 @@
-type stringOrNumber = string | number
+type One = string
+type Two = string | number
+type Three = 'hello'
 
-type stringOrNumberArray = (string | number)[]
+// Convert to more or less specific
 
+let a: One = "hello"
+let b = a as Two // less specific
+let c = a as Three
 
-interface Guitarist {
-    name ?: string,
-    active: boolean,
-    albums: stringOrNumberArray
+let d = <One> 'world'
+let e = <string | number> 'world'
+
+const addOrrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
+    if(c === "add") return a + b
+    return '' + a + b
 }
 
-type UserId = stringOrNumber
+let myVal: string = addOrrConcat(2,2, 'concat') as string
 
-// literal types
+let nextVal: number =  addOrrConcat(2,2, 'concat') as number
 
-let myName: "Dave"
+// The DOM
 
-let UserName: "Dave" | "John" | "Amy"
-UserName = "Amy"
+const img = document.querySelector('img') as HTMLImageElement
+const myImg = document.getElementById('#img') as HTMLImageElement
+const nextImg = <HTMLImageElement>document.getElementById('#img')
 
-// function 
-const add = (a: number,b:number): number => {
-    return a + b
-}
+img.src
+myImg.src 
 
-console.log(add(10,20))
-
-const logMsg = (message: any):void => {
-    console.log(message)
-}
-
-logMsg("Hello!")
-
-
-let subtract = function (c: number, d: number): number {
-    return c - d
-}
-
-// type mathFunction = (a: number, b: number) => number
-
-interface mathFunction {
-    (a: number, b: number): number
-}
-
-let multiply: mathFunction = function (c,d) {
-    return c * d
-}
-
-logMsg(multiply(20,20))
-
-
-// optional parameter
-const addAll = (a: number, b: number, c?: number):number => {
-    if( typeof c !== "undefined") {
-        return a + b + c 
-    }   
-    return a + b
-}
-
-
-const sumAll = (a: number = 20, b: number, c: number = 10): number => {
-    return a + b + c
-}
-
-
-logMsg(addAll(2,2,2))
-logMsg(add(2,2))
-logMsg(sumAll(2,2))
-logMsg(sumAll(undefined, 3))
-
-console.log("===========")
-
-// Rest Parameter
-const total = (a:number, ...nums: number[]): number => {
-    return a + nums.reduce((prev, curr) => prev + curr)
-}
-
-logMsg(total(10,2,3,4,5))
-
-
-const isNumber = (value: any):boolean => {
-    return typeof value === 'number'
-        ? true: false
-}
-
-const numberOrString = (value: number | string) => {
-    if(typeof value === 'string') {
-        return "This is the string"
-    }   
-    if(typeof value === 'number') {
-        return "This is the number"
-    }
-}
 
