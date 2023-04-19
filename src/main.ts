0810bd9a7805fc98ca9ad06d84883c3b3bcb0825
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-19 11:47:37 +0700

    add file modifile

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,124 @@
-type One = string
-type Two = string | number
-type Three = 'hello'
+// Class 
 
-// Convert to more or less specific
-
-let a: One = "hello"
-let b = a as Two // less specific
-let c = a as Three
-
-let d = <One> 'world'
-let e = <string | number> 'world'
-
-const addOrrConcat = (a: number, b: number, c: 'add' | 'concat'): number | string => {
-    if(c === "add") return a + b
-    return '' + a + b
+class Coder {
+    constructor (
+        public readonly name: string,
+        public music: string,
+        private age: number,
+        protected lang: string = "TypeScript"
+    ) {
+        this.name = name
+        this.music = music
+        this.age = age
+        this.lang = lang
+    }
+    public getAge() {
+        return `Hello, I'm ${this.age}`
+    }
 }
 
-let myVal: string = addOrrConcat(2,2, 'concat') as string
+const Dave = new Coder("Dave", "Rock", 42)
 
-let nextVal: number =  addOrrConcat(2,2, 'concat') as number
+console.log(Dave)
+console.log(Dave.getAge())
 
-// The DOM
+class WebDev extends Coder {
+    constructor(
+        public computer: string,
+        name: string,
+        music: string,
+        age: number,
+    ) {
+        super(name, music, age)
+        this.computer = computer
+    }
+    
+    public getLang() {
+        return `I write ${this.lang}`
+    }
+    
+}
 
-const img = document.querySelector('img') as HTMLImageElement
-const myImg = document.getElementById('#img') as HTMLImageElement
-const nextImg = <HTMLImageElement>document.getElementById('#img')
+const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25) 
+console.log(Sara)
+console.log(Sara.getLang()) 
 
-img.src
-myImg.src 
+// Interface
+
+interface Musician {
+    name: string,
+    instrument: string,
+    play(action: string): string
+}
+
+class Guitarist implements Musician {
+    name: string
+    instrument: string
+
+    constructor(name: string, instrument: string) {
+        this.name = name
+        this.instrument = instrument
+    }
+    
+    play(action: string) {
+        return `${this.name} ${action} the ${this.instrument}`
+    }
+}
+
+const Page = new Guitarist('Jimmy', 'guitar')
+console.log(Page.play('strums'))
+
+// 
+
+class Peeps {
+    static count: number = 0
+
+    static getCount(): number {
+        return Peeps.count
+    }
+    
+    public id: number
+
+    constructor(public name: string) {
+        this.name = name
+        this.id = ++Peeps.count
+    }
+}
+
+const John = new Peeps('John')
+const Steve = new Peeps('Steve')
+const Amy = new Peeps('Amy')
 
 
+console.log(John.id)
+console.log(Steve.id)
+console.log(Amy.id)
+
+console.log(Peeps.count)
+
+////////////////////////////////////
+
+class Bands {
+    private dataState: string[]
+    
+    constructor() {
+        this.dataState = []
+    }
+    
+    public get data(): string[] {
+        return this.dataState
+    }
+
+    public set data(value: string[]) {
+        if(Array.isArray(value) && value.every(el => typeof el === 'string')) {
+            this.dataState = value
+            return 
+        } else throw new Error ('Param is not an array of strings')
+    }
+}
+
+const MyBands = new Bands()
+MyBands.data = ['Neil Young', 'Led Zep', 'Mac']
+console.log(MyBands.data)
+MyBands.data = [...MyBands.data, 'ZZ Top']
+MyBands.data = ['Van Hale']
