1ee0b3adf8410634d157e9a82e402e8e9dedba8c
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-20 21:53:06 +0700

    add file modifile

--- src/main.ts
+++ src/main.ts
@@ -1,124 +1,146 @@
-// Class 
+// Example 1
+function convert<T,U> (input: T, converter:(input: T) => U):U {
+    return converter(input);
+}
 
-class Coder {
-    constructor (
-        public readonly name: string,
-        public music: string,
-        private age: number,
-        protected lang: string = "TypeScript"
-    ) {
-        this.name = name
-        this.music = music
-        this.age = age
-        this.lang = lang
+const str = '123'
+const num = convert(str, Number); // num = 123
+
+console.log(num)
+
+// Example 2
+
+function sort<T>(array: T[], compareFn: (a: T, b: T) => number): T[] {
+    return array.sort(compareFn)
+}
+
+const numbers = [3,1,4,1,5,9,2,6,5,3,5]
+const sorted = sort(numbers,(a,b) => a - b)
+console.log(sorted)
+
+// Generics
+const echo = <T>(arg: T):T => arg
+
+const isObj = <T>(arg: T): boolean => {
+    return (typeof arg === 'object' && !Array.isArray(arg) && arg !==null)
+}
+
+console.log(isObj(true))
+console.log(isObj('John'))
+console.log(isObj([1,2,3]))
+console.log(isObj({name: 'John'}))
+console.log(isObj(null))
+
+
+const isTrue = <T>(arg: T): {arg: T, is: boolean} => {
+    if(Array.isArray(arg) && !arg.length) {
+        return {arg, is: false}
     }
-    public getAge() {
-        return `Hello, I'm ${this.age}`
+    if(isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return {arg, is:false}
+    }
+    return {arg, is: !!arg}
+}
+
+console.log(isTrue(false))
+console.log(isTrue(0))
+console.log(isTrue(true))
+console.log(isTrue(1))
+
+
+
+interface BoolCheck<T> {
+    value: T,
+    is: boolean,
+}
+
+const checkBoolValue  = <T>(arg: T): BoolCheck<T> => {
+    if(Array.isArray(arg) && !arg.length) {
+        return {value: arg, is: false}
+    }
+    if(isObj(arg) && !Object.keys(arg as keyof T).length) {
+        return {value: arg, is:false}
+    }
+    return {value:arg, is: !!arg}
+}
+
+interface HasID {
+    id: number
+}
+
+const processUser = <T extends HasID>(user: T): T => {
+    return user
+}
+
+console.log(processUser({id: 1, name: "Dave"}))
+
+const getUsersProperty = <T extends HasID, K extends keyof T> (users: T[], key: K): T[K][] => {
+    return users.map(user => user[key])
+}
+
+
+
+function generic<M>(arg: M):M {
+    return arg;
+}
+
+let output1 = generic<string>("I am learning")
+let output2 = generic<boolean>(true)
+let output3 = generic<number> (145)
+let output4 = generic<string> ("毎日ITを勉強してる頑張りましょう")
+
+console.log(output1)
+console.log(output2)
+console.log(output3)
+console.log(output4)
+
+console.log("====")
+
+class Queue<T> {
+    private data: T[] = []
+    
+    push(item: T) {
+        this.data.push(item);
+    }
+
+    pop(): T | undefined {
+        return this.data.shift();
     }
 }
 
-const Dave = new Coder("Dave", "Rock", 42)
+let q = new Queue<string>()
 
-console.log(Dave)
-console.log(Dave.getAge())
+q.push("日本")
+q.push("英語")
 
-class WebDev extends Coder {
-    constructor(
-        public computer: string,
-        name: string,
-        music: string,
-        age: number,
-    ) {
-        super(name, music, age)
-        this.computer = computer
-    }
-    
-    public getLang() {
-        return `I write ${this.lang}`
-    }
-    
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+console.log(q.pop());
+
+console.log("=====")
+
+interface Pair <C,H,> {
+    firstName: C;
+    Age: H
+    Addrest: C
 }
 
-const Sara = new WebDev('Mac', 'Sara', 'Lofi', 25) 
-console.log(Sara)
-console.log(Sara.getLang()) 
+let pair1: Pair<string,number> = {firstName: "Yushing", Age: 21, Addrest:"ITを勉強してる"}
+let pair2: Pair<string,boolean> = {firstName: "Guen", Age: false, Addrest: "Vn"}
 
-// Interface
+console.log(pair1)
+console.log(pair2)
 
-interface Musician {
-    name: string,
-    instrument: string,
-    play(action: string): string
+console.log("====")
+
+interface Lengthwise {
+    length: number;
 }
 
-class Guitarist implements Musician {
-    name: string
-    instrument: string
-
-    constructor(name: string, instrument: string) {
-        this.name = name
-        this.instrument = instrument
-    }
-    
-    play(action: string) {
-        return `${this.name} ${action} the ${this.instrument}`
-    }
+function logLength<T extends Lengthwise>(arg: T): void {
+    console.log(arg.length);
 }
 
-const Page = new Guitarist('Jimmy', 'guitar')
-console.log(Page.play('strums'))
-
-// 
-
-class Peeps {
-    static count: number = 0
-
-    static getCount(): number {
-        return Peeps.count
-    }
-    
-    public id: number
-
-    constructor(public name: string) {
-        this.name = name
-        this.id = ++Peeps.count
-    }
-}
-
-const John = new Peeps('John')
-const Steve = new Peeps('Steve')
-const Amy = new Peeps('Amy')
-
-
-console.log(John.id)
-console.log(Steve.id)
-console.log(Amy.id)
-
-console.log(Peeps.count)
-
-////////////////////////////////////
-
-class Bands {
-    private dataState: string[]
-    
-    constructor() {
-        this.dataState = []
-    }
-    
-    public get data(): string[] {
-        return this.dataState
-    }
-
-    public set data(value: string[]) {
-        if(Array.isArray(value) && value.every(el => typeof el === 'string')) {
-            this.dataState = value
-            return 
-        } else throw new Error ('Param is not an array of strings')
-    }
-}
-
-const MyBands = new Bands()
-MyBands.data = ['Neil Young', 'Led Zep', 'Mac']
-console.log(MyBands.data)
-MyBands.data = [...MyBands.data, 'ZZ Top']
-MyBands.data = ['Van Hale']
+logLength("頑張ってください")
+logLength([1,2,4,5,6])
