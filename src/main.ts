45dd4e6a47cdd822c5276b94e136c54b4461d68d
Huu Hung <huuhungnguyen2002@gmail.com>
2023-04-24 22:47:34 +0700

    add file modifile

--- src/main.ts
+++ src/main.ts
@@ -1,200 +1,100 @@
-interface Person {
-    name: string;
-    age: number;
-    address: string;
+// Partial<Type>
+// Utility Type Partial<Type> cho phép bạn tạo ra một phiên bản mới của một interface hoặc một kiểu dữ liệu nào đó, nhưng tất cả các thuộc tính của nó đều có thể bị thiếu.
+
+interface Todo {
+    title: string;
+    fullName: string;
+    myPhone: number
 }
 
-type PartialPerson = Partial<Person>;
-
-const Person: PartialPerson = {name: "John", age: 21, address: "毎日ITを勉強してる頑張ってください"}
-console.log(Person)
-
-
-interface Person2 {
-    name2 ?: string;
-    age2 ?: number;
-    address2 ?: string;
+function updateTodo(todo: Todo, fieldsToUpdate: Partial<Todo>) {
+    return {...todo, ...fieldsToUpdate}
 }
 
-type RequiredPerson = Required<Person2>;
-const person2 : RequiredPerson = { name2: "Yushing", age2: 22, address2: "毎日ITを勉強してる"}
+const todo1 = {
+    title: "organize desk",
+    fullName: "Yushing",
+    myPhone: 1
+}
 
-console.log(person2)
+const todo2 = updateTodo(todo1, {
+    fullName: "Guenfufun",
+    myPhone: 2
+})
 
-console.log("===========")
-// NonNullable
+console.log(todo2)
+
+
 interface User {
-    fistName: string;
-    lastName: string;
-    age ?: number;
-    address ?: string | null;
+    name: string;
+    email: string;
+    phone: string;
 }
 
-type NonNullableUser = NonNullable<User>
+type PartialUser = Partial<User>;
 
-const user: NonNullableUser = {
-    fistName: "John",
-    lastName: "Doe",
-    address: "HN, VN",
-    id: 0,
-    name: "",
-    username: "",
-    email: ""
+const user: PartialUser = {
+    name: "Yushing",
+    email: "huuhungnguyen2002@gmail.com"
 }
-
 console.log(user)
 
-console.log("======")
+// Required<Type>
+// Utility Type Required<Type> cho phép bạn tạo ra một phiên bản mới của một interface hoặc một kiểu dữ liệu nào đó, nhưng tất cả các thuộc tính của nó đều phải được cung cấp.
 
-function add(a: number, b: number): number {
-    return a + b
+interface User2 {
+    name?: string;
+    email?: string;
+    phone?: string;
 }
 
-type AddReturnType = ReturnType<typeof add>
-const result: AddReturnType = 20
+type RequiredUser = Required<User>
 
-console.log(result)
-
-// Utility Type
-console.log("=====")
-
-// Partial cho phép bạn tạo một phiên bản mới của một kiểu dữ liệu với tất cả các thuộc tính đều là optional:
-
-interface Assignment {
-    studentId: string,
-    title: string,
-    grade: number,
-    verified ?: boolean,
+const user2: RequiredUser = {
+    name: "John Doe",
+    email: 'yushing02@gmail.com',
+    phone: "09123312332"
 }
 
-const updateAssignment = (assign: Assignment, propsToUpdate: Partial<Assignment>): Assignment => {
-    return {...assign, ...propsToUpdate}
+console.log(user2)
+
+// ReadOnly Không thể gán lại
+// Utility Type Readonly<Type> cho phép bạn tạo ra một phiên bản mới của một interface hoặc một kiểu dữ liệu nào đó, nhưng tất cả các thuộc tính của nó đều là thuộc tính chỉ đọc.
+
+interface Only {
+    readonly name: string;
+    readonly address: string;
+    readonly email: string;
 }
 
-const assign1: Assignment = {
-    studentId: "compsci123",
-    title: "Final Projesct", 
-    grade: 0,
-    verified : false,
+
+const only: Only = {
+    name: "John Doe",
+    address: 'Yushing',
+    email: "huuhungnguyen2002@gmail.com",
 }
 
-const assignGraded : Assignment = updateAssignment(assign1, { grade: 95})
-console.log(assignGraded)
+// only.name = "yshing" Lỗi: Thuộc tính "name" không thể được gán lại
 
-
-// Required and ReadOnly
-const recordAssignment = (assign: Required<Assignment>): Assignment => {
-    return assign
-}
-
-const assignVerified: Readonly<Assignment> = {
-    ...assignGraded, verified: true
-}
-
-recordAssignment({...assignGraded, verified: true})
+console.log(only)
 
 // Record
-const hexColorMap: Record<string, string> = {
-    red: "FF000",
-    green: "00FF00",
-    blue:"000FF"
+// Utility Type Record<KeyType, ValueType> cho phép bạn tạo ra một kiểu dữ liệu mới, trong đó các thuộc tính được xác định bởi các giá trị của KeyType, và các giá trị của các thuộc tính đó được xác định bởi ValueType.
+
+
+interface CarInfo {
+    age: number;
+    breed: string;
 }
 
-type Students = "Sara" | "Kelly"
-type LetterGrades = "A" | "B" | "C" | "D" | "U" 
+type CatName = "miffy" | 'boris' | 'mordred';
 
-const LetterGrades: Record<Students, LetterGrades> = {
-    Sara: "B",
-    Kelly: "U"
+const cats: Record<CatName, CarInfo> = {
+    miffy: {age: 10, breed: "Yushing Name"},
+    boris: {age: 20, breed: "Face"},
+    mordred: {age: 60, breed: "Fonme"}
+    
 }
 
-interface Grades {
-    assign1: number,
-    assign2: number,
-}
+console.log(cats)
 
-const gradeData: Record<Students, Grades> = {
-        Sara: {assign1: 85, assign2: 93},
-        Kelly: {assign1: 76, assign2: 15},
-}
-
-console.log(gradeData)
-
-// Pick and Omit
-
-type AssignResult = Pick<Assignment, "studentId" | "grade">
-
-const score: AssignResult = {
-    studentId: "k123",
-    grade: 85,
-}
-
-console.log(score)
-
-type AssigPreview = Omit<Assignment, "grade" | "verified">
-
-const priview: AssigPreview = {
-    studentId: "k123",
-    title: "Final Project"
-}
-
-console.log(priview)
-
-// Exclude and Extract
-
-type adjustedGrade = Exclude<LetterGrades, "U">
-
-
-type hightGrades = Extract<LetterGrades, "A" | "B">
-
-// Nonnullable
-
-type AllPossibleGrades = "Dave" | "John" | null | undefined
-
-type NamesOnly = NonNullable<AllPossibleGrades>
-
-// ReturnType
-
-//type newAssign = {title: string, points: number}
-
-const createNewAssign = (title: string, points: number, fullCourse: string, address: string) => {
-    return {title, points, fullCourse, address}
-}
-
-type NewAssign = ReturnType<typeof createNewAssign>
-
-const tsAssign: NewAssign = createNewAssign("ITを勉強してる頑張りましょう", 100, "2 triệu", "ITが難しいです")
-console.log(tsAssign)
-
-// Parameter
-
-type AssignParams = Parameters<typeof createNewAssign>
-
-const assignArgs: AssignParams = [ "Generics", 200, "4tr", "日本語を勉強して英語を勉強してる"]
-
-const tsAssign2: NewAssign = createNewAssign(...assignArgs)
-console.log(tsAssign2)
-
-// Awaited - helps us width the ReturnType of a Promise 
-
-interface User {
-    id: number,
-    name: string,
-    username: string,
-    email: string,
-}
-
-const fetchUser = async(): Promise<User[]> => {
-    const data = await fetch (
-        'https://jsonplaceholder.typicode.com/users'
-    ).then(res => {
-        return res.json()
-    }).catch(err => {
-        if(err instanceof Error) console.log(err.message)
-    })
-    return data
-}
-
-type FetchUsersReturnType = Awaited<ReturnType<typeof fetchUser>>
-
-fetchUser().then(users => console.log(users))
